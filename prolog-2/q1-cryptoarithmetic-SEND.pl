2eadc92ae64165bb3d2229b84d356ff17f33d536
Sudip Tiwari <sudip10063@gmail.com>
2024-01-07 17:38:19 +0545

    add comments in send more money

--- prolog-2/q1-cryptoarithmetic-SEND.pl
+++ prolog-2/q1-cryptoarithmetic-SEND.pl
@@ -1,22 +1,30 @@
 % SEND MORE MONEY problem
+% run with query ?- solution([S,E,N,D,M,O,R,Y]).
 
-:- use_module(library(clpfd)).
+:- use_module(library(clpfd)). % for constraint operation #=
 
 solution([S,E,N,D,M,O,R,Y]):-
     C4=1, %c4 must be 1 otherwise no need to mention M.
+
+    % Uncomment the lines below one by one till it does not show "Time Limit Exceeded"
     E=5,
     O=0,
-    % Uncomment the lines below one by one till it does not show "Time Limit Exceeded"
     % D=7,
     % M=1,
     % N=6,
     % R=8,
     % S=9,
     % Y=2,
+
+    % member is a built in predicate in SWI Prolog with definition:
+    % member(X, [X|_]).
+	% member(X, [_|T]) :- member(X, T).
+
     member(C1,[0,1]),
     member(C2,[0,1]),
     member(C3,[0,1]),
     % C1, C2, C3 will have values 0 or 1
+
     member(E,[0,1,2,3,4,5,6,7,8,9]),
     member(N,[0,1,2,3,4,5,6,7,8,9]),
     member(D,[0,1,2,3,4,5,6,7,8,9]),
@@ -27,9 +35,11 @@ solution([S,E,N,D,M,O,R,Y]):-
     member(S,[0,1,2,3,4,5,6,7,8,9]),
     % S,E,N, D, M, O, R, Y will have values between 0 and 9.
     % The values of S,E,N, D, M, O, R, Y must not be equal.
+    
     all_different([S,E,N,D,M,O,R,Y]),
+
     % Computation for solution
-    D+E #= Y + 10*C1,
+    D+E #= Y + 10*C1, % if you write =(Assignment operator), it gives false
     N+R+C1 #= E + 10*C2,
     E+O+C2 #= N + 10*C3,
     S+M+C3 #= O + 10*C4,
